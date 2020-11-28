b7f5fb908a183b7e48d2ce9e11363b08e30bc70c
Mansour Ahmadi <mansourweb@gmail.com>
2020-11-28 13:19:34 -0500

    add apt-get update to install script

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+sudo apt-get update
 sudo apt-get -y install clang-3.8
 sudo apt-get -y install clang-6.0
 sudo apt-get -y install llvm-3.8
@@ -10,7 +11,6 @@ sudo apt-get -y install python-dev graphviz libgraphviz-dev pkg-config
 sudo apt-get -y install python-pip
 pip install --upgrade pip
 pip install -r requirements.txt
-git submodule update --init
 cd dg
 cmake -D CMAKE_C_COMPILER="/usr/bin/clang-6.0" -D CMAKE_CXX_COMPILER="/usr/bin/clang++-6.0" .
 make
