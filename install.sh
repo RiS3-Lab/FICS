110698cbfaa27589880ca083478c09ff774dac72
Mansour Ahmadi <mansourweb@gmail.com>
2020-11-28 15:35:13 -0500

    mongodb installation commands to install script

--- install.sh
+++ install.sh
@@ -15,3 +15,8 @@ pip install -r requirements.txt
 cd dg
 cmake -D CMAKE_C_COMPILER="/usr/bin/clang-6.0" -D CMAKE_CXX_COMPILER="/usr/bin/clang++-6.0" .
 make
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org=4.4.2 mongodb-org-server=4.4.2 mongodb-org-shell=4.4.2 mongodb-org-mongos=4.4.2 mongodb-org-tools=4.4.2
+sudo systemctl start mongod
