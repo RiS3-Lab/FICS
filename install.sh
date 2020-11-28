5637b73b7894820dc4d2e94e5f733fdf1adf9c6e
Mansour Ahmadi <mansourweb@gmail.com>
2020-11-28 14:56:44 -0500

    undate install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ sudo apt-get -y install libpng-dev libfreetype6-dev
 sudo apt-get -y install python-dev graphviz libgraphviz-dev pkg-config
 sudo apt-get -y install python-pip
 pip install --upgrade pip
+pip install --upgrade setuptools
 pip install -r requirements.txt
 cd dg
 cmake -D CMAKE_C_COMPILER="/usr/bin/clang-6.0" -D CMAKE_CXX_COMPILER="/usr/bin/clang++-6.0" .
